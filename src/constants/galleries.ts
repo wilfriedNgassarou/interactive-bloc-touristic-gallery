3691e751121683dd955fcc4b0a54c91a96713576
unknown <wilfriedngassarou23@gmail.com>
2025-02-26 00:47:03 +0100

    Added galeries constants

---
+++ src/constants/galleries.ts
@@ -0,0 +1,46 @@
+export const galleries = [
+  {
+    id: 'site-touristique',
+    backgroundColor: '#B79470',
+    name: 'Site touristique',
+    description: 'Explorez les meilleurs lieux touristiques et historique du Cameroun',
+    mainImage: '/images/2.png',
+  },
+  {
+    id: 'nord-cameroun',
+    backgroundColor: '#BBB7FC',
+    name: 'Nord Cameroun',
+    description: 'Explorez les meilleurs lieux touristiques et historique du Cameroun',
+    mainImage: null,
+    cards: [
+      {
+        id: 'nord-cameroun-card1',
+        backgroundColor: '#E0E1EC',
+        title: 'Nord Cameroun',
+        description: 'Les gorges de Kola sont situées sur le territoire de la commune de Guider dans le Nord Cameroun.',
+        image: 'images/3.png'
+      },
+      {
+        id: 'nord-cameroun-card2',
+        backgroundColor: '#FFCE61',
+        title: 'Nord Cameroun',
+        description: 'Les gorges de Kola sont situées sur le territoire de la commune de Guider dans le Nord Cameroun.',
+        image: 'images/4.png'
+      },
+      {
+        id: 'nord-cameroun-card3',
+        backgroundColor: '#A7DAA0',
+        title: 'Nord Cameroun',
+        description: 'Les gorges de Kola sont situées sur le territoire de la commune de Guider dans le Nord Cameroun.',
+        image: 'images/5.png'
+      },
+    ]
+  },
+  {
+    id: 'ouest-bangoulap',
+    backgroundColor: '#FDCE63',
+    name: 'Ouest, Bangoulap',
+    description: 'Maison d\'hôtes de charme, la villa Boutanga est située sur le site de la Fondation Jean-Felicien',
+    mainImage: '/images/1.png'
+  },
+]
---
+++ src/constants/size.ts
@@ -0,0 +1,5 @@
+export const size = {
+  GALLERY_WIDTH: 384,
+  GALLERY_HEIGHT: 500,
+  GAP: 16
+}
